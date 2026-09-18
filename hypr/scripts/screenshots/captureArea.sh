497dd1f0352982502d6fc6e0f621e19e45f198bd
Ritvik-Kaza <ritvik.kaza@gmail.com>
2026-09-18 19:12:16 +0530

    hypr: don't save/notify on cancelled screenshot area selection

--- hypr/scripts/screenshots/captureArea.sh
+++ hypr/scripts/screenshots/captureArea.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 DIR="$(xdg-user-dir PICTURES)/Screenshots"
 mkdir -p "$DIR"
+GEOM="$(slurp)"
+[ -z "$GEOM" ] && exit 0
 FILE="$DIR/$(date +'%Y-%m-%d_%H-%M-%S').png"
-grim -g "$(slurp)" "$FILE"
+grim -g "$GEOM" "$FILE"
 wl-copy < "$FILE"
 notify-send "Screenshot saved" "$FILE"
